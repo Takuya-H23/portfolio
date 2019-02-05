787ce477bbb6cce39d3a5c2d9a7ec55c7c4e1e7a
takuya <takuyahirata4@gmail.com>
2019-02-05 14:15:55 -0500

    added about section and nav

--- js/app.js
+++ js/app.js
@@ -13,6 +13,22 @@
 (function() {
     'use strict';
 
-    
+    // Header Nav Animation
+    const navElement = document.getElementById('headerNav');
+    const hamburgerMenu = document.getElementById('hamburgerMenu');
+    const xIcon = document.getElementById('xIcon');
+
+    hamburgerMenu.addEventListener('click', () => {
+        TweenMax.to(navElement, 1, {
+            x: -400
+        });
+    });
+
+    xIcon.addEventListener('click', () => {
+        TweenMax.to(navElement, 1.5, {
+            x: 400
+        });
+    });
+
 
 }());
