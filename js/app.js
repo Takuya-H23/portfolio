1bbe8a7088906d73a03cd61389c4d9ea4ac532ae
takuya <takuyahirata4@gmail.com>
2019-02-12 12:07:27 -0500

    added portfolio fieatures

--- js/app.js
+++ js/app.js
@@ -13,14 +13,61 @@
 (function() {
     'use strict';
 
-    // Header Nav Animation
+    // Header Nav Section
     const navElement = document.getElementById('headerNav');
     const hamburgerMenu = document.getElementById('hamburgerMenu');
     const xIcon = document.getElementById('xIcon');
 
+    //Portfolio Section
+    const titleList = document.getElementById('titleList');
+    const portfolioWrapper = document.getElementById('portfolioWrapper');
+    const portfolio = [];
     // Form Section
     const submitBtn = document.getElementById('submitBtn');
 
+    class Portfolio {
+        constructor(title, images, description, skills) {
+            this.title = title;
+            this.images = images;
+            this.description = description;
+            this.skills = skills;
+        }
+    }
+
+    portfolio.push(new Portfolio(
+        'MAD XSCAPE ROOM',
+        ['./img/rps.png', './img/memorycard.png'],
+        'MAD Xcsape Room project for Durham College. I was responsible for creating dynamic leaderborad page.',
+        ['HTML', 'CSS', 'JavaScript', 'jQuery', 'Photoshop']
+    ));
+
+    portfolio.push(new Portfolio(
+        'JS GAMING',
+        ['./img/rps.png', './img/memorycard.png'],
+        'Rock Paper Scissors and Memory Card Game.',
+        ['HTML', 'CSS', 'SASS', 'JavaScript', 'jQuery', 'Illustrator']
+    ));
+
+    portfolio.push(new Portfolio(
+        'WEB DEVELOPMENT INFOGRAPHIC',
+        ['./img/rps.png', './img/memorycard.png'],
+        'Infographic project to show what front-end web developers do.',
+        ['HTML', 'CSS', 'JavaScript', 'jQuery', 'Illustrator']
+    ));
+
+    portfolio.push(new Portfolio(
+        'LAKERIDGE HEALTH',
+        ['./img/rps.png', './img/memorycard.png'],
+        'Data visualization project to display Lakeridge projects.',
+        ['HTML', 'CSS', 'JavaScript', 'jQuery', 'D3']
+    ));
+
+    portfolio.push(new Portfolio(
+        'CYBER CHAT APPLICATION',
+        ['./img/rps.png', './img/memorycard.png'],
+        'Cybar chat application using Node.js',
+        ['HTML', 'CSS', 'JavaScript', 'Node.js', 'Express']
+    ));
     /**
      *  Hamburger Menu Animation
      */
@@ -54,19 +101,45 @@
     });
 
     /**
-     * Hover Animation for Portfolio Description
+     * Hover Animation for Portfolio 
      */
-    $('.overlay').on('mouseover', e => {
-        TweenMax.to(e.target, .5, {
-            opacity: 1
+    titleList.addEventListener('mouseover', (e) => {
+        let elementTitle = e.target.innerText;
+        let target = portfolio.filter(item => item.title === elementTitle);
+        let title = target[0].title;
+        let portfolioHTML = '';
+
+        portfolioHTML = `<h4>${title}</h4>`;
+        portfolioHTML += '<div class="thumbnail-wrapper">';
+        target[0].images.forEach(image => {
+            portfolioHTML += `<div><img src="${image}" alt="${title}"></div>`; 
         });
+        portfolioHTML += `
+            </div>
+            <p class="desc">${target[0].description}</p>
+            <div class="skills-wrapper">
+        `;
+        target[0].skills.forEach(skill => {
+            portfolioHTML += `<span>${skill}</span>`;
+        });
+        portfolioHTML += '</div>';
+
+        portfolioWrapper.innerHTML = portfolioHTML;
     });
 
-    $('.overlay').on('mouseout', e => {
-        TweenMax.to(e.target, .5, {
-            opacity: 0
-        });
-    });
+    // <h4>JS GAMING</h4>
+    //     <div class="thumbnail-wrapper">
+    //         <div><img src="./img/memorycard.png" alt=""></div>
+    //             <div><img src="./img/rps.png" alt=""></div>
+    //             </div>
+    //             <p class="desc">Christmas Memory Card Game and Rock Paper Scissors Game.</p>
+    //             <div class="skills-wrapper">
+    //                 <span>HTML</span>
+    //                 <span>CSS</span>
+    //                 <span>JavaScript</span>
+    //                 <span>jQuery</span>
+    //                 <span>Illustrator</span>
+    //             </div>
 
     /**
      * Ajax for Sending Email
